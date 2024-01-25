a94632e0f802b24307ee3bfdf653fa2c41bf7e0f
LeeSeongWon <tjddnjs10617@naver.com>
2024-01-25 15:26:37 +0900

    add user_exists for email Validation

--- app/register.py
+++ app/register.py
@@ -1,11 +1,14 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from .models import Member, RoleEnum, StatusEnum, GradeEnum
 from .utils import hash_password
 from pydantic import BaseModel
 from datetime import datetime
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker
 from config import ASYNC_DATABASE_URI
+from sqlalchemy.future import select
+from .database import AsyncSessionLocal, get_db, async_engine
+app = FastAPI()
+
 
 # Pydantic 모델 정의 - 사용자 입력을 검증하기 위함
 class RegisterRequest(BaseModel):
@@ -13,21 +16,31 @@ class RegisterRequest(BaseModel):
     nickname: str
     password: str
 
-app = FastAPI()
 # 비동기 데이터베이스 엔진 생성
-engine = create_async_engine(ASYNC_DATABASE_URI)
+engine = async_engine
 # 비동기 세션 생성자
-AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+AsyncSessionLocal = AsyncSessionLocal
+
+# `user_exists` 함수를 사용하여 이메일 주소로 사용자 존재 여부 확인
+async def user_exists(email: str, db_session: AsyncSession) -> bool:
+    async with db_session as session:
+        # `Member` 모델에서 이메일 주소로 사용자를 조회
+        result = await session.execute(select(Member).filter(Member.email == email))
+        member = result.scalars().first()
+        return member is not None
 
 @app.post('/register')
-async def register_member(request: RegisterRequest):
+async def register_member(request: RegisterRequest, db: AsyncSession = Depends(get_db)):
     # 입력 데이터 검증
     email = request.email
     nickname = request.nickname
     password = request.password
 
+    if await user_exists(request.email, db):
+        raise HTTPException(status_code=400, detail="이미 존재하는 사용자입니다.")
+
     if not email or not nickname or not password:
-        raise HTTPException(status_code=400, detail="모든 필드를 입력해야 합니다.")
+        raise HTTPException(status_code=400, detail="모든 칸을 입력해주세요")
 
     # 비밀번호 해싱
     hashed_pwd = hash_password(password)
